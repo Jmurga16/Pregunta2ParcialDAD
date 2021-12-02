a6cda1d287104b7ce7493e07e31978d9cce8abc1
Jmurga16 <58633633+Jmurga16@users.noreply.github.com>
2021-12-02 05:28:49 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,73 @@
+
+var datosGlobales;
+
+//#region Inicializar
+$(document).ready(function () {
+
+
+  $("#sRuc").on('change', onChangeDocument);
+
+});
+//#endregion
+
+//#region Cambiar Numero de Documento
+function onChangeDocument() {
+  if ($(this).val() != 0) {
+
+    var sDocumento;
+    //console.log($(this).val());
+
+    sDocumento = $(this).val()
+
+    var formData = new FormData();
+    formData.append("token", "2UNujeomioME3I3m9fM7mmngxA9AhJBI0mBVzCf7h4yOq0uCFlZg2L500jvT");
+    formData.append("ruc", sDocumento);
+    var request = new XMLHttpRequest();
+
+
+
+    request.open("POST", "https://api.migo.pe/api/v1/ruc");
+
+
+    request.setRequestHeader("Accept", "application/json");
+    request.send(formData);
+    request.onload = function () {
+      var data = JSON.parse(this.response);
+      console.log(data);
+
+      datosGlobales = data;
+
+      llenarDatos(JSON.stringify(datosGlobales))
+      //$('#dataJSON').val(JSON.stringify(datosGlobales));
+
+    };
+
+
+
+  }
+
+
+
+
+}
+//#endregion
+
+function llenarDatos(datosGlobales) {
+  let contenido = '';
+  let datoVacio = '';
+  let html_tbody = '';
+  let ContadorFila = 0;
+
+  contenido = $('#divContent');
+  contenido.empty();
+
+  html_tbody = "<p>";
+
+  html_tbody = datosGlobales;
+
+  html_tbody += "</p>";
+
+  contenido.append(html_tbody);
+
+
+}
